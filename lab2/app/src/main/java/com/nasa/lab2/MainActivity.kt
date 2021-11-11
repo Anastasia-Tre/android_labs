a8317f5cd9681c856c6534ec14022d42a09cf04f
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-11-11 19:29:04 +0200

    add logic of fragments

--- lab2/app/src/main/java/com/nasa/lab2/MainActivity.kt
+++ lab2/app/src/main/java/com/nasa/lab2/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.nasa.lab2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FontSelectFragment.OnFontSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onFontSelected(text: String, font: String) {
+        val fragment = supportFragmentManager
+            .findFragmentById(R.id.textResultFragment) as TextResultFragment
+
+        fragment.setTextResult(text, font)
     }
 }
--- lab2/app/src/main/java/com/nasa/lab2/TextResultFragment.kt
+++ lab2/app/src/main/java/com/nasa/lab2/TextResultFragment.kt
@@ -1,9 +1,11 @@
 package com.nasa.lab2
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 
@@ -12,8 +14,13 @@ class TextResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_text_result, container, false)
+    }
+
+    fun setTextResult(text: String, font: String) {
+        val tv_result = requireView().findViewById<TextView>(R.id.tv_result)
+        tv_result.typeface = Typeface.create(font, Typeface.NORMAL)
+        tv_result.text = text + font
     }
 
 }
