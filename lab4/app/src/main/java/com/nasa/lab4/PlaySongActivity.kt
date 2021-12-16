d1f1380295b99a57d9c632d8716be712b80c8c79
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-12-16 18:54:30 +0200

    add logic to play song activity

--- lab4/app/src/main/java/com/nasa/lab4/PlaySongActivity.kt
+++ lab4/app/src/main/java/com/nasa/lab4/PlaySongActivity.kt
@@ -1,11 +1,147 @@
 package com.nasa.lab4
 
-import android.os.Bundle
+import android.annotation.SuppressLint
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 
 class PlaySongActivity : AppCompatActivity() {
+
+    private lateinit var mp: MediaPlayer
+    private var totalTime: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_play_song)
+        setContentView(R.layout.activity_main)
+
+        val uri: Uri = Uri.parse(intent.getStringExtra("uri"))
+        mp = MediaPlayer().apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build()
+            )
+            setDataSource(applicationContext, uri)
+            prepare()
+            start()
+        }
+        //mp = MediaPlayer.create(this, R.raw.music)
+        mp.isLooping = true
+        mp.setVolume(0.5f, 0.5f)
+        totalTime = mp.duration
+
+
+        val playBtn = findViewById<Button>(R.id.playBtn)
+        playBtn.setOnClickListener {
+            if (mp.isPlaying) {
+                //Stop
+                mp.pause()
+                it.setBackgroundResource(R.drawable.play)
+            } else {
+                //Start
+                mp.start()
+                it.setBackgroundResource(R.drawable.stop)
+            }
+        }
+
+        // Volume Bar
+        val volumeBar = findViewById<SeekBar>(R.id.volumeBar)
+        volumeBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) {
+                        val volumeNum = progress / 100.0f
+                        mp.setVolume(volumeNum, volumeNum)
+                    }
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+            }
+        )
+
+        // PositionBar
+        val positionBar = findViewById<SeekBar>(R.id.positionBar)
+        positionBar.max = totalTime
+        positionBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) {
+                        mp.seekTo(progress)
+                    }
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+            }
+        )
+
+        // Thread
+        Thread(Runnable {
+            while (true) {
+                try {
+                    val msg = Message()
+                    msg.what = mp.currentPosition
+                    handler.sendMessage(msg)
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                }
+            }
+        }).start()
     }
+
+    @SuppressLint("HandlerLeak")
+    var handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            val currentPosition = msg.what
+
+            // Update positionBar
+            val positionBar = findViewById<SeekBar>(R.id.positionBar)
+            positionBar.progress = currentPosition
+
+            // Update Labels
+            val elapsedTimeLabel = findViewById<TextView>(R.id.elapsedTimeLabel)
+            val elapsedTime = createTimeLabel(currentPosition)
+            elapsedTimeLabel.text = elapsedTime
+
+            val remainingTimeLabel = findViewById<TextView>(R.id.remainingTimeLabel)
+            val remainingTime = createTimeLabel(totalTime - currentPosition)
+            remainingTimeLabel.text = "-$remainingTime"
+        }
+    }
+
+    fun createTimeLabel(time: Int): String {
+        var timeLabel = ""
+        val min = time / 1000 / 60
+        val sec = time / 1000 % 60
+
+        timeLabel = "$min:"
+        if (sec < 10) timeLabel += "0"
+        timeLabel += sec
+
+        return timeLabel
+    }
+
 }
